136dac67d72ffd56e21d4fa5dd1167c70561ea8f
TiaMia23 <166423118+TiaMia23@users.noreply.github.com>
2026-02-16 17:57:32 -0500

    Add ReactionStepExecutor and ReactionStep scripts

---
+++ Assets/Scripts/Gameplay/Coop/Reactions/ReactionStep.cs
@@ -0,0 +1,20 @@
+using Unity.Android.Gradle;
+using UnityEngine;
+
+namespace Gameplay.Items
+{
+    [System.Serializable]
+    public class ReactionStep
+    {
+        public string StepName;
+
+        public float WindowStart = 0f;
+        public float WindowEnd = 0f;
+
+        public float StepDuration = 5f;
+
+        public LabItem FullYieldItem;
+        public LabItem DownGradedItem;
+    }
+}
+
