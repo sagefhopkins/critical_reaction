54ed3ff165ffc5197f01aa3a16e5e4c87d9d985b
TiaMia23 <166423118+TiaMia23@users.noreply.github.com>
2026-02-24 22:33:28 -0500

    Fixed WasteReason merge conflict

--- Assets/Scripts/Gameplay/Workstations/Waste/WasteReason.cs
+++ Assets/Scripts/Gameplay/Workstations/Waste/WasteReason.cs
@@ -10,10 +10,5 @@ namespace Gameplay.Workstations
         WrongOrder = 1,
         MissedTimeWindow = 2,
         Overheated = 4,
-<<<<<<< HEAD
-
-        All = WrongOrder | MissedTimeWindow | Overheated
-=======
->>>>>>> b0ce7ae1bff843602fba15f53fc0d593c9880ef2
     }
 }
